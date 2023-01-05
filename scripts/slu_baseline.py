117ef563051242fc717854247ab1fbaebc45ddd9
INTE <1062142886@qq.com>
2023-01-05 21:18:33 +0800

    Update slu_baseline.py

--- scripts/slu_baseline.py
+++ scripts/slu_baseline.py
@@ -1,5 +1,6 @@
 #coding=utf8
 import sys, os, time, gc
+from xpinyin import Pinyin
 from torch.optim import Adam
 
 install_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -52,11 +53,9 @@ def set_optimizer(model, args):
 
 
 def decode(choice):
-    assert choice in ['train', 'dev', 'test']
+    assert choice in ['train', 'dev']
     model.eval()
     dataset = train_dataset if choice == 'train' else dev_dataset
-    if choice == 'test':
-        dataset = test_dataset
     predictions, labels = [], []
     total_loss, count = 0, 0
     with torch.no_grad():
@@ -71,10 +70,71 @@ def decode(choice):
             labels.extend(label)
             total_loss += loss
             count += 1
+        predictions = anti_noise_prediction(predictions)
         metrics = Example.evaluator.acc(predictions, labels)
     torch.cuda.empty_cache()
     gc.collect()
     return metrics, total_loss / count
+
+def anti_noise_prediction(predictions):
+    p = Pinyin()
+    select_pos_set = {'poi名称', 'poi修饰', 'poi目标', '起点名称', '起点修饰', '起点目标', '终点名称', '终点修饰', '终点目标', '途经点名称'}
+    select_others_set = {'请求类型': [Example.label_vocab.request_map_dic, Example.label_vocab.request_pinyin_set], \
+        '出行方式' : [Example.label_vocab.travel_map_dic, Example.label_vocab.travel_pinyin_set], \
+        '路线偏好' : [Example.label_vocab.route_map_dic, Example.label_vocab.route_pinyin_set], \
+        '对象' :  [Example.label_vocab.object_map_dic, Example.label_vocab.object_pinyin_set], \
+        '页码' : [Example.label_vocab.page_map_dic, Example.label_vocab.page_pinyin_set], \
+        '操作' : [Example.label_vocab.opera_map_dic, Example.label_vocab.opera_pinyin_set], \
+        '序列号' : [Example.label_vocab.ordinal_map_dic, Example.label_vocab.ordinal_pinyin_set]   }
+
+    modify_num = 0
+    for i, pred in enumerate(predictions):
+        pred_length = len(pred)
+        if pred_length > 0 :
+            for j in range(pred_length):
+                tmp_pred = pred[j]
+                split_result = tmp_pred.split('-')
+                tmp_pinyin = p.get_pinyin(split_result[2], ' ')
+                if split_result[1] != 'value' :
+                    if split_result[1] in select_pos_set :
+                        map_dic, pinyin_set = Example.label_vocab.poi_map_dic, Example.label_vocab.poi_pinyin_set
+                    else :
+                        [map_dic, pinyin_set] = select_others_set[split_result[1]]
+
+                    standard_output = get_standard_output (map_dic, pinyin_set, tmp_pinyin)
+                    modify_pred = split_result[0] + '-' + split_result[1] + '-' + standard_output
+                    if standard_output != split_result[2] :
+                        modify_num += 1
+                        print ("standard_output = ", standard_output, " split_result[2] = ", split_result[2])
+                    predictions[i][j] = modify_pred
+    print ("modify_num == ", modify_num)                    
+    return  predictions            
+
+def get_standard_output (map_dic, pinyin_set, tmp_pinyin) :
+    if tmp_pinyin in pinyin_set :
+        standard_output = map_dic[tmp_pinyin]
+    else :
+        max_similarity = 0
+        most_similar_pinyin = ''
+        for standard_pinyin in iter(pinyin_set) :
+            similarity = get_pinyin_similarity(standard_pinyin, tmp_pinyin)
+            if similarity > max_similarity :
+                max_similarity = similarity
+                most_similar_pinyin = standard_pinyin
+        if max_similarity == 0 : 
+            standard_output = '无'
+        else :
+            standard_output = map_dic[most_similar_pinyin]
+    return standard_output
+            
+
+def get_pinyin_similarity(standard_pinyin, tmp_pinyin) :
+    standard_set = set (standard_pinyin.split(' '))
+    tmp_set = set (tmp_pinyin.split(' '))
+
+    inter_set = standard_set & tmp_set
+    similarity = len (inter_set) / (len (standard_set) + len (tmp_set) )
+    return similarity
 
 
 if not args.testing:
